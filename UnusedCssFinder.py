e997b6f3b6911b167c76077ee36f31d269f74d84
Harderer <jannisharder@hotmail.de>
2016-08-12 08:48:44 +0200

    Added further functionality
    check changelog for information

--- UnusedCssFinder.py
+++ UnusedCssFinder.py
@@ -1,65 +1,111 @@
-import sublime, sublime_plugin, os, re, string, time
+import sublime, sublime_plugin, os, re, string, time, json
 from os import listdir
 
 UCF_IS_ACTIVE = {}
 ALLOWED_EXTENSIONS = ["php", "html", "xhtml", "js"]
 
 class UnusedCssFinderCommand(sublime_plugin.TextCommand):
-	
+
 	def run(self, edit, **args):
 		self.debug = args['debug'] if 'debug' in args else False
 		sublime.set_timeout_async(self.async_search, 1)
 
 	def load_plugin_setting(self, setting_name):
 
-		# get plugin settings
-		settings_plugin = sublime.load_settings('UnusedCssFinder.sublime-settings')
-		setting_value_plugin = settings_plugin.get(setting_name)
-		if setting_value_plugin != None and setting_value_plugin != "":
-			return setting_value_plugin
+		plugin_setting_value = None
 
-		return None
+		# get plugin settings
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		setting_value = plugin_settings.get(setting_name)
+		if setting_value != None and setting_value != "":
+			plugin_setting_value = setting_value
+
+		# get settings from plugin configuration file in folder
+		if self.project_settings is not False:
+			if setting_name in self.project_settings:
+				if(isinstance(self.project_settings[setting_name], list) and isinstance(plugin_setting_value, list)):
+					for value in self.project_settings[setting_name]:
+						plugin_setting_value.append(value)
+				elif(isinstance(self.project_settings[setting_name], dict) and isinstance(plugin_setting_value, dict)):
+					for (key, value) in self.project_settings[setting_name].items():
+						plugin_setting_value[key] = value
+				else:
+					plugin_setting_value = self.project_settings[setting_name]
+
+		return plugin_setting_value
 
 	def async_search(self):
 		global UCF_IS_ACTIVE
-		
-		view = self.view
-		regions = [sublime.Region(0, view.size())]
+
+		regions = [sublime.Region(0, self.view.size())]
 
 		filename = self.view.file_name()
-		ignoreFolders = []
-		scanOnlyFolders = False
 
-		project_rootpath = self.get_active_project_path(filename)
-		setting_identifier = re.sub('[\W_]', '', project_rootpath)
+		self.project_rootpath = unused_css_get_active_project_path(filename)
+		self.ignoreFolders = []
+		self.scanOnlyFolders = False
+		self.ignoreSelectors = []
+		self.autoDelete = False
+		self.hightlightUnusedSelectors = False
+
+		setting_identifier = re.sub('[\W_]', '', self.project_rootpath)
 
 		# initialize project plugin status if necessary
-		if(project_rootpath not in UCF_IS_ACTIVE):
-			UCF_IS_ACTIVE[project_rootpath] = False
+		if(self.project_rootpath not in UCF_IS_ACTIVE):
+			UCF_IS_ACTIVE[self.project_rootpath] = False
+
+		self.project_settings = False
+		project_settings_filename = os.path.join(self.project_rootpath, "unused_css.cfg")
+		if os.path.isfile(project_settings_filename):
+			try:
+				self.project_settings = json.loads(open(project_settings_filename).read())
+			except ValueError:
+				self.view.show_popup("Info: JSON data couldn't be loaded from "+project_settings_filename)
 
 		# get settings
 		st_rootFolder = self.load_plugin_setting('unused_css_root_folder')
 		st_ignoreFolders = self.load_plugin_setting('unused_css_ignore_folders')
 		st_scanOnlyFolders = self.load_plugin_setting('unused_css_scan_only_folders')
+		st_ignoreSelectors = self.load_plugin_setting('unused_css_ignore_selectors')
+		st_autoDelete = self.load_plugin_setting('unused_css_delete_on_search')
+		st_hightlightUnusedSelectors = self.load_plugin_setting('unused_css_highlight_selectors')
 
 		if st_rootFolder != None and st_rootFolder != "":
-			project_rootpath = st_rootFolder
+			self.project_rootpath = st_rootFolder
 		if st_ignoreFolders != None and st_ignoreFolders != "":
-			ignoreFolders = st_ignoreFolders
+			self.ignoreFolders = st_ignoreFolders
 		if st_scanOnlyFolders != None and st_scanOnlyFolders != "":
-			scanOnlyFolders = st_scanOnlyFolders
+			self.scanOnlyFolders = st_scanOnlyFolders
+		if st_ignoreSelectors != None and st_ignoreSelectors != "":
+			self.ignoreSelectors = st_ignoreSelectors
+		if st_autoDelete != None:
+			self.autoDelete = st_autoDelete
+		if st_hightlightUnusedSelectors != None:
+			self.hightlightUnusedSelectors = st_hightlightUnusedSelectors
 
-		highlight_size = int(view.settings().get('highlight_size_'+setting_identifier, 0))
+		highlight_size = int(self.view.settings().get('highlight_size_'+setting_identifier, 0))
 		ufIsActive = False
 
-		if not UCF_IS_ACTIVE[project_rootpath] or highlight_size <= 0:
-			self.search_areas(setting_identifier, filename, project_rootpath, ignoreFolders, scanOnlyFolders)
+		self.remove_highlighting(self.view, setting_identifier, highlight_size)
+
+		if not UCF_IS_ACTIVE[self.project_rootpath] or highlight_size <= 0 or self.hightlightUnusedSelectors is False:
+			self.view.sel().clear()
+
+			self.search_areas(setting_identifier, filename)
 			ufIsActive = True
+
+			if self.autoDelete:
+				self.view.run_command("left_delete")
 		else:
-			tests = self.remove_highlighting(view, setting_identifier, highlight_size)
 			ufIsActive = False
 
-		UCF_IS_ACTIVE[project_rootpath] = ufIsActive
+		# workaround for viewport bug
+		vpos = self.view.viewport_position()
+		self.view.set_viewport_position((vpos[0], vpos[1]+1))
+		self.view.set_viewport_position((vpos[0], vpos[1]-1))
+		####
+
+		UCF_IS_ACTIVE[self.project_rootpath] = ufIsActive
 
 	def remove_highlighting(self, view, setting_identifier, highlight_size):
 		for i in range(highlight_size):
@@ -68,10 +114,10 @@ class UnusedCssFinderCommand(sublime_plugin.TextCommand):
 
 		view.settings().set('highlight_size_'+setting_identifier, 0)
 
-	def search_areas(self, setting_identifier, filename, project_rootpath, ignoreFolders, scanOnlyFolders):
+	def search_areas(self, setting_identifier, filename):
 		# get all class and ids from file
-		fileContent = open(filename).read().lstrip().replace('\r', ' ').replace('\n', ' ')
-		fileContent = re.sub('{[^}]*}', '', fileContent)	# remove all content between brackets
+		self.file_content = self.view.substr(sublime.Region(0, self.view.size()))
+		file_content_stripped = re.sub('{[^}]*}', '', self.file_content.lstrip().replace('\r', ' ').replace('\n', ' '))	# remove all content between brackets
 
 		fileExtension = os.path.splitext(filename)[1][1:]
 
@@ -79,24 +125,26 @@ class UnusedCssFinderCommand(sublime_plugin.TextCommand):
 		unusedHits = 0
 
 		if fileExtension == "css":
-			words = fileContent.split(" ")	# determine all words in file
+			words = file_content_stripped.split(" ")	# determine all words in file
 			word_length = len(words)
 
-			unusedHits += self.search_words(setting_identifier, filename, project_rootpath, ignoreFolders, scanOnlyFolders, words, word_count, word_length, unusedHits, False)
+			unusedHits += self.search_words(setting_identifier, filename, words, word_count, word_length, unusedHits, False)
 		else:
 			# css inside file, search only in current file
-			cssContents = re.findall('<style[^>]*>([^(<)]*)', fileContent)	# determine all contents between style tags
+			cssContents = re.findall('<style[^>]*>([^(<)]*)', file_content_stripped)	# determine all contents between style tags
 			words = []
 			for (index, content) in enumerate(cssContents):
 				words.extend(content.split(" "))		# determine all words in contents
 			word_length = len(words)
 
-			unusedHits += self.search_words(setting_identifier, filename, project_rootpath, ignoreFolders, scanOnlyFolders, words, word_count, word_length, unusedHits, True)
+			unusedHits += self.search_words(setting_identifier, filename, words, word_count, word_length, unusedHits, True)
 			
 		self.view.settings().set('highlight_size_'+setting_identifier, unusedHits)
 		sublime.status_message("{:6.2f}%".format(100)+": "+str(unusedHits)+" unused css names found")
 
-	def search_words(self, setting_identifier, filename, project_rootpath, ignoreFolders, scanOnlyFolders, words, word_count, word_length, unusedHits, css_inside_file):
+	def search_words(self, setting_identifier, filename, words, word_count, word_length, unusedHits, css_inside_file):
+
+		searched_selectors = []
 		
 		for word in words:
 			word = word.strip(' \t\n\r')
@@ -107,45 +155,56 @@ class UnusedCssFinderCommand(sublime_plugin.TextCommand):
 					classIdNameSplitted = re.sub('[.:[](.)*', '', word[1:]).replace(',', ' ').split(" ")
 					for classIdName in classIdNameSplitted:
 						classIdName = classIdName.strip(' \t\n\r')
-						if classIdName != "":
+						if classIdName != "" and classIdName not in searched_selectors:
 
-							if(self.debug):
-								print("search for '"+classIdName+"' in")
-							# search all files in folder and subfolders for given class or id name
-							appearanceFound = self.search_in_folder(project_rootpath, filename, classIdName, ignoreFolders, scanOnlyFolders, css_inside_file)
+							searched_selectors.append(classIdName)
 
-							# highlight name if appearance was not found
-							if not appearanceFound:
+							if ((word[0:1]+classIdName) not in self.ignoreSelectors and classIdName not in self.ignoreSelectors):
 								if(self.debug):
-									print(">> no match found")
+									print("search for '"+classIdName+"' in")
+								# search all files in folder and subfolders for given class or id name
+								appearanceFound = self.search_in_folder(self.project_rootpath, filename, classIdName, css_inside_file)
 
-								regions = self.view.find_all('(?<!\w)('+word+')(?!\w)', sublime.IGNORECASE)
-								self.view.add_regions('highlight_word_%d'%unusedHits, regions, 'invalid')
+								# highlight name if appearance was not found
+								if not appearanceFound:
+									if(self.debug):
+										print(">> no match found")
 
-								unusedHits+=1
+									# regions = self.view.find_all('(?<!\w)('+word+')(?!\w).*(?<=\{).*(?<=\})', sublime.IGNORECASE)
+									matches = re.finditer(re.compile('(?<!\w)('+word+')(?!\w)[^{]*{[^}]*}', re.DOTALL), self.file_content)
 
-							if(self.debug):
-								print("-----------")
+									if self.hightlightUnusedSelectors:
+										self.view.add_regions('highlight_word_%d'%unusedHits, self.view.find_all('(?<!\w)('+word+')(?!\w)', sublime.IGNORECASE), 'invalid')
+									
+									if not self.hightlightUnusedSelectors or self.autoDelete:
+										for match in matches:
+											region = sublime.Region(match.start(0), match.end(0))
+											self.view.sel().add(region)
+
+									unusedHits+=1
+
+								if(self.debug):
+									print("-----------")
 
 			word_count+=1
 			sublime.status_message("{:6.2f}%".format((word_count/word_length)*100)+": "+str(unusedHits)+" unused css names found")
 
 		return unusedHits
 
-	def search_in_folder(self, folderpath, trigger_filename, search_for, ignoreFolders, scanOnlyFolders, css_inside_file):
+	def search_in_folder(self, folderpath, trigger_filename, search_for, css_inside_file):
 		appearanceFound = False
-		if scanOnlyFolders == False or len(scanOnlyFolders) == 0 or folderpath in scanOnlyFolders:
+		if self.scanOnlyFolders == False or len(self.scanOnlyFolders) == 0 or folderpath in self.scanOnlyFolders:
 			for f in listdir(folderpath):
 				filepath = os.path.join(folderpath, f)
 
 				if(os.path.isfile(filepath)):
 					appearanceFound = self.search_in_file(filepath, trigger_filename, search_for, css_inside_file)
-				if(os.path.isdir(filepath) and filepath not in ignoreFolders):
-					if scanOnlyFolders != False and len(scanOnlyFolders) > 0 and scanOnlyFolders[folderpath] == True:
+				if(os.path.isdir(filepath) and filepath not in self.ignoreFolders):
+					if self.scanOnlyFolders != False and len(self.scanOnlyFolders) > 0 and self.scanOnlyFolders[folderpath] == True:
 						# scan only folder setting says to scan all subfolders aswell
-						appearanceFound = self.search_in_folder(filepath, trigger_filename, search_for, ignoreFolders, False, css_inside_file)
+						appearanceFound = self.search_in_folder(filepath, trigger_filename, search_for, self.ignoreFolders, False, css_inside_file)
 					else:
-						appearanceFound = self.search_in_folder(filepath, trigger_filename, search_for, ignoreFolders, scanOnlyFolders, css_inside_file)
+						appearanceFound = self.search_in_folder(filepath, trigger_filename, search_for, css_inside_file)
 
 				if appearanceFound:
 					if(self.debug):
@@ -170,18 +229,82 @@ class UnusedCssFinderCommand(sublime_plugin.TextCommand):
 					return False
 			return False
 	
-	def get_active_project_path(self, filename):
-		window = sublime.active_window()
-		folders = window.folders()
-		if len(folders) == 1:
-			return folders[0]
-		else:
-			active_view = window.active_view()
-			active_file_name = active_view.file_name() if active_view else None
-			
-			if not active_file_name:
-				return folders[0] if len(folders) else os.path.dirname(filename)
-			for folder in folders:
-				if active_file_name.startswith(folder):
-					return folder
-		return os.path.dirname(active_file_name)
+def unused_css_get_active_project_path(filename):
+	window = sublime.active_window()
+	folders = window.folders()
+	if len(folders) == 1:
+		return folders[0]
+	else:
+		active_view = window.active_view()
+		active_file_name = active_view.file_name() if active_view else None
+		
+		if not active_file_name:
+			return folders[0] if len(folders) else os.path.dirname(filename)
+		for folder in folders:
+			if active_file_name.startswith(folder):
+				return folder
+	return os.path.dirname(active_file_name)
+
+
+class addToIgnoreListCommand(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		selected_region = self.view.sel()[0]
+		if selected_region.begin() != selected_region.end():
+			add_to_ignore = self.view.substr(sublime.Region(selected_region.begin(), selected_region.end()))
+			if add_to_ignore != "":
+				plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+				ignoreSelectors = plugin_settings.get('unused_css_ignore_selectors')
+				if add_to_ignore not in ignoreSelectors:
+					ignoreSelectors.append(add_to_ignore)
+					plugin_settings.set('unused_css_ignore_selectors', ignoreSelectors)
+					sublime.save_settings('UnusedCssFinder.sublime-settings')
+	def is_enabled(self):
+		selected_region = self.view.sel()[0]
+		if selected_region.begin() != selected_region.end():
+			add_to_ignore = self.view.substr(sublime.Region(selected_region.begin(), selected_region.end()))
+			if add_to_ignore != "":
+				return True
+
+		return False
+		
+class checkHighlightSelectors(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		plugin_settings.set('unused_css_highlight_selectors', True)
+		sublime.save_settings('UnusedCssFinder.sublime-settings')
+
+		filename = self.view.file_name()
+		project_rootpath = unused_css_get_active_project_path(filename)
+		UCF_IS_ACTIVE[project_rootpath] = False
+
+	def is_checked(self):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		return (plugin_settings.get('unused_css_highlight_selectors') is True)
+		
+
+class uncheckHighlightSelectors(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		plugin_settings.set('unused_css_highlight_selectors', False)
+		sublime.save_settings('UnusedCssFinder.sublime-settings')
+	def is_checked(self):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		return (plugin_settings.get('unused_css_highlight_selectors') is not True)
+		
+class checkAutoDelete(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		plugin_settings.set('unused_css_delete_on_search', True)
+		sublime.save_settings('UnusedCssFinder.sublime-settings')
+	def is_checked(self):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		return (plugin_settings.get('unused_css_delete_on_search') is True)
+
+class uncheckAutoDelete(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		plugin_settings.set('unused_css_delete_on_search', False)
+		sublime.save_settings('UnusedCssFinder.sublime-settings')
+	def is_checked(self):
+		plugin_settings = sublime.load_settings('UnusedCssFinder.sublime-settings')
+		return (plugin_settings.get('unused_css_delete_on_search') is not True)
